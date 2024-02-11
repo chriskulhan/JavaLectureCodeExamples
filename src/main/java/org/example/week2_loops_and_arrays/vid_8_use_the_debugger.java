03d3f35f28b9a086e064982d54980f9557c23dd9
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2024-02-11 17:39:31 -0600

    finished video 8 debugger

---
+++ src/main/java/org/example/week2_loops_and_arrays/vid_8_use_the_debugger.java
@@ -0,0 +1,37 @@
+//It is invaluable for fixing logic errors with your code
+//If your code is crashing or giving you unexpected output/behavior,
+// try running it in the debugger
+//It's likely a variable has a value you don't expect; or that it's not
+// following the path of execution you expect (e.g. it's taking the else
+// branch when you expect it to follow the if branch)
+//The debugger will help you fix many logic errors with your code
+
+//**Setting breakpoints:
+//Very useful if your code is crashing or if you have a logic error that you can't figure out
+//Set a breakpoint just before the code that is causing problems
+//You can set more than one breakpoint if you like
+//Now, when you debug your code, the debugger will pause execution when your code gets to that line
+//Very helpful for testing or trying to find and fix logic errors
+
+//**Other resources can help if debugger doesn't:
+//Internet
+//Stack Overflow, http://stackoverflow.com
+//Make sure you search for answers before posting a new question
+//Tutors and the Academic Success Center!!
+//Study groups
+//Ask me! In class, come to office hours, email to set up an appointment
+//Java Documentation (occasionally cryptic,
+// sometimes helpful) https://docs.oracle.com/en/java/javase/14/docs/api/index.html
+
+//**Prevent errors with good variable names:
+//If you have variables called mile, miles, numberMiles, m, number
+//Can you remember which one is which?
+//If you use the wrong variable, you'll probably have a bug
+
+package org.example.week2_loops_and_arrays;
+
+public class vid_8_use_the_debugger {
+    public static void main(String[] args) {
+
+    }
+}
