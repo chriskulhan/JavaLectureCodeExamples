09e909a011c3260bfac9b565fa4bbb8f82ee3fb5
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2024-02-17 12:03:53 -0600

    made changes?

---
+++ src/main/java/org/example/week2_loops_and_arrays/week_2_homework_fiddling.java
@@ -0,0 +1,23 @@
+package org.example.week2_loops_and_arrays;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class week_2_homework_fiddling {
+    public static void main(String[] args) {
+
+        // Some example String arrays, to help test the printProgramFiles method
+
+        String[] exampleFilenames1 = {"lab1.py", "assignment.docx", "music.mp3", "Website.cs", "Calculator.java"};
+        printProgramFiles(exampleFilenames1);   // This should print lab1.py, Website.cs and Calculator.java, one per line
+
+        String[] exampleFilenames2 = {"accounting.xlsx", "lab2.py", "Arrays.java"};
+        printProgramFiles(exampleFilenames2);   // This should print lab2.py and Arrays.java, one per line
+    }
+
+    public static void printProgramFiles(String[] filenames) {
+        for (int x = 0; x < filenames.length; x++) {
+            System.out.println(filenames[x]);
+    }
+
+    }
+}
