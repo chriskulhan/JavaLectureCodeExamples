35a052ad89afd191e93e8d3e63d3e4795283433b
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2024-02-24 16:38:05 -0600

    got vid_1 example to work

--- src/main/java/org/example/week3_methods_program_design/vid_1_warm_up_2_running_schedule.java
+++ src/main/java/org/example/week3_methods_program_design/vid_1_warm_up_2_running_schedule.java
@@ -15,24 +15,65 @@ What type of loop do you need? A for loop, or a while loop?
 package org.example.week3_methods_program_design;
 
 public class vid_1_warm_up_2_running_schedule {
+    public static void main(String[] args) {
 
-    //my attempt: (wrong)
-//    String [] distancePerWeek = {};
-//    for (double runningDistance = 1.0, runningDistance <= 26.2, runningDistance = runningDistance*(1.10)); {
-//        System.out.println(distancePerWeek[runningDistance]);
+//        my attempt: (wrong)
+//        String[] distancePerWeek = {};
+//        for (double runningDistance = 1.0, runningDistance <=26.2, runningDistance = runningDistance * (1.10));
+//        {
+//            System.out.println(distancePerWeek[runningDistance]);
 
-    //Clara's work:
-
-    //variables:
-    double currentDistance = 1; //starting point before loop
-    double targetDistance = 26.2; //max miles they want to run
-    double percentIncrease = 1.10; //(increase by 10%)
-    int week = 1;
-
-    while (currentDistance < targetDistance) {
-        System.out.println(week + "" + currentDistance);
-        currentDistance = currentDistance * percentIncrease;
+            //Clara's work:
+//        //variables:
+//        double currentDistance = 1; //starting point before loop
+//        double targetDistance = 26.2; //max miles they want to run
+//        double percentIncrease = 1.10; //(increase by 10%)
+//        int week = 1;
+//
+//        //loop will count the number of weeks.
+//        while (currentDistance < targetDistance) {
+//            System.out.println(week + " " + currentDistance);
+//            currentDistance = currentDistance * percentIncrease;
+//            week++; //count number of weeks
+//        }
+//        System.out.println(week + " " + currentDistance);
+        }
     }
-}
+//output: 1 1.0
+//2 1.1
+//3 1.2100000000000002
+//4 1.3310000000000004
+//5 1.4641000000000006
+//6 1.6105100000000008
+//7 1.771561000000001
+//8 1.9487171000000014
+//9 2.1435888100000016
+//10 2.357947691000002
+//11 2.5937424601000023
+//12 2.853116706110003
+//13 3.1384283767210035
+//14 3.4522712143931042
+//15 3.797498335832415
+//16 4.177248169415656
+//17 4.594972986357222
+//18 5.054470284992944
+//19 5.559917313492239
+//20 6.115909044841463
+//21 6.72749994932561
+//22 7.400249944258172
+//23 8.140274938683989
+//24 8.954302432552389
+//25 9.849732675807628
+//26 10.834705943388391
+//27 11.91817653772723
+//28 13.109994191499954
+//29 14.420993610649951
+//30 15.863092971714948
+//31 17.449402268886445
+//32 19.19434249577509
+//33 21.1137767453526
+//34 23.22515441988786
+//35 25.54766986187665
+//36 28.102436848064315
 
-**changes to make pushing happen
+
---
+++ src/main/java/org/example/week3_methods_program_design/vid_2_java_methods.java
@@ -0,0 +1,18 @@
+/*Java methods;
+
+Very much like functions in Python
+You may have written code like this
+Remember function definitions, arguments and parameters, return values
+Code in a function doesn't run until it is called
+
+ */
+
+package org.example.week3_methods_program_design;
+
+public class vid_2_java_methods {
+    //main method: (runs first)
+    public static void main(String[] args) {
+
+    }
+
+}
