2a47998198b9599638ad44e7e93d544e34ef87d5
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2024-02-24 16:53:58 -0600

    through vid 2. pause

--- src/main/java/org/example/week3_methods_program_design/vid_2_java_methods.java
+++ src/main/java/org/example/week3_methods_program_design/vid_2_java_methods.java
@@ -9,10 +9,43 @@ Code in a function doesn't run until it is called
 
 package org.example.week3_methods_program_design;
 
+import static input.InputUtils.stringInput;
+
 public class vid_2_java_methods {
-    //main method: (runs first)
-    public static void main(String[] args) {
+    //Clara's code:
+    // main method: (runs first)
 
-    }
+        //This is the main method - the first code to run
+        public static void main(String[] args) {
 
-}
+            String name = stringInput("Please enter your name");
+            String greeting = makeGreeting(name);
+            // name is the argument ^^there, it's what will become "(String n)" below in the
+            //other place where makeGreeting is.
+            System.out.println(greeting);
+
+        }    // This is the end of the main method.
+
+
+        // A new method that we've created.
+        // This method takes one argument, a String.
+        // It will create a new String made from the word "Hello"
+        // plus the String argument, plus an exclamation point !
+        // It will then return that new greeting String.
+        public static String makeGreeting(String n) {
+            //when makeGreeting has a () down here it's called a parameter ^^^
+            String greeting = "Hello " + n + "!";
+            return greeting;
+
+        }    // The end of the makeGreeting method
+
+    }   // End of the vid_2_java_methods class
+
+//input/output:
+//Please enter your name
+//chris
+//Hello chris!
+
+
+
+
